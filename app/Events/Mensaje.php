a1deac7507e476a33afc8b1d0e6623f8d57af3cb
Dvid19 <davidmatiasdm19@gmail.com>
2025-11-21 09:43:35 -0600

    Commit 5

---
+++ app/Events/Mensaje.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class Mensaje implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $mensaje;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($mensaje)
+    {
+        //
+        $this->mensaje = $mensaje;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            // new PrivateChannel('channel-name'),
+            new Channel('');
+        ];
+    }
+}
--- routes/channels.php
+++ routes/channels.php
@@ -6,3 +6,7 @@ Broadcast::channel('game.{id}', function ($user, $id) {
     // return (int) $user->id === (int) $id;
     return ['id' => $user->id ?? null, 'name' => $user->name ?? null];
 });
+
+Broadcast::channel('chat.{id}', function($user, $id) {
+    return (int) $user->id === (int) $id;
+});
